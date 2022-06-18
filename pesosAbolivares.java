32d33c10ab0fb6d8333f0041062e7289b3813983
paolabr4 <90522319+paolabr4@users.noreply.github.com>
2022-06-18 09:06:16 -0500

    Creacion del proyecto Pesos a Bolivares cambios1

--- pesosAbolivares.java
+++ pesosAbolivares.java
@@ -1,3 +1,25 @@
 public class pesosAbolivares {
 
+    double pesos = 0;
+    double valorBol = 0.0014;
+    double bolivares = 0;
+
+    public pesosAbolivares() {
+        // TODO Auto-generated constructor stub
+
+        JOptionPane.showMessageDialog(null, "Convertidor de Pesos a Bolivares");
+        pesos = JOptionPane.showInputDialog("Porfavor ingreses el valor en pesos: ");
+
+        bolivares = pesos * valorBol;
+
+        JptionPane.showMessageDialog(null, pesos + " pesos, equivalen a " + bolivares + " bolivares");
+
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        new pesosAbolivares();
+
+    }
 }
